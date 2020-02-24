120e0471829de1c4cb671f244cbb84f8bbdb19e4
zabaas <36841576+zabaas@users.noreply.github.com>
2020-02-24 14:29:25 +0800

    Update Address.sol

--- contracts/utils/Address.sol
+++ contracts/utils/Address.sol
@@ -38,7 +38,7 @@ library Address {
      *
      * _Available since v2.4.0._
      */
-    function toPayable(address account) internal pure returns (address payable) {
+    function toPayable(address account) internal pure returns (address) {
         return address(uint160(account));
     }
 
@@ -60,7 +60,7 @@ library Address {
      *
      * _Available since v2.4.0._
      */
-    function sendValue(address payable recipient, uint256 amount) internal {
+    function sendValue(address recipient, uint256 amount) internal {
         require(address(this).balance >= amount, "Address: insufficient balance");
 
         // solhint-disable-next-line avoid-low-level-calls, avoid-call-value
